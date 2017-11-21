5f53e984dc722de9925a1c6beaab14174cbdca6e
NICO-LAPTOP\Nico <nico.mazek@googlemail.com>
2017-11-21 14:43:53 +0100

    Initial Commit

---
+++ Ball Brawl Project/Assets/Scripts/NetworkManagerScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkManagerScript : NetworkBehaviour {
+
+    public GameObject ballPrefab;
+
+    public override void OnStartServer() {
+        GameObject ball = Instantiate(ballPrefab, new Vector3(0f, 5f, 0f), Quaternion.identity);
+        NetworkServer.Spawn(ball);
+    }
+}
---
+++ Ball Brawl Project/Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerControler : NetworkBehaviour {
+
+    private Rigidbody _rigidbody;
+
+    private void Awake() {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    public override void OnStartLocalPlayer() {
+        Camera camera = GetComponentInChildren<Camera>();
+        camera.enabled = true;
+    }
+
+    public void Update() {
+        if (!isLocalPlayer) return;
+
+        ProcessControls();
+    }
+
+    private void ProcessControls() {
+        if (Input.GetKey(KeyCode.W)) {
+            Debug.Log("w");
+            _rigidbody.AddRelativeForce(new Vector3(0, 0, 5));
+        } else if (Input.GetKey(KeyCode.S)) {
+            Debug.Log("s");
+            _rigidbody.AddRelativeForce(new Vector3(0, 0, -5));
+        }
+
+        if(Input.GetKey(KeyCode.A)) {
+            Debug.Log("a");
+            _rigidbody.AddRelativeForce(new Vector3(-5, 0, 0));
+        } else if(Input.GetKey(KeyCode.D)) {
+            Debug.Log("d");
+            _rigidbody.AddRelativeForce(new Vector3(5, 0, 0));
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space)) {
+            _rigidbody.AddRelativeForce(new Vector3(0, 500, 0));
+            Debug.Log("jump");
+        }
+    }
+}
