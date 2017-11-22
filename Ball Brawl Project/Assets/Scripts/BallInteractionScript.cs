4eaeb3dda48d9ed3912788991f4da3e740c16157
NICO-LAPTOP\Nico <nico.mazek@googlemail.com>
2017-11-22 12:46:49 +0100

    Early networking prototype

---
+++ Ball Brawl Project/Assets/Scripts/BallInteractionScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class BallInteractionScript : NetworkBehaviour {
+
+    public void Update() {
+        if (!isLocalPlayer) return; //only checks for the locally controlling player
+
+        RaycastHit hit;
+
+        if(Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, 10f)) {
+            if (hit.collider.gameObject.tag == Tags.BALL) Debug.Log("Ball in range");
+        }
+    }
+
+    private void ProcessMouseInput() {
+        if(Input.GetMouseButton(0)) {
+            //Do stuff
+        }
+    }
+}
--- Ball Brawl Project/Assets/Scripts/PlayerControllerRigidbody.cs
+++ Ball Brawl Project/Assets/Scripts/PlayerControllerRigidbody.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.Networking;
+using UnityStandardAssets.CrossPlatformInput;
 using System.Collections;
 
 [RequireComponent(typeof(Rigidbody))]
@@ -14,34 +15,62 @@ public class PlayerControllerRigidbody : NetworkBehaviour {
     public float jumpHeight = 2.0f;
     private bool grounded = false;
 
+    public float sensitivityX = 10f;
+    private float rotationX;
+    private Quaternion originalRotation;
+
     private Rigidbody rigidbody;
-    private CameraController camController;
+    private Camera cam;
+
+    public override void OnStartLocalPlayer() {
+        //Init the local player
+        GetComponentInChildren<Camera>().enabled = true;
+        GetComponentInChildren<AudioListener>().enabled = true;
+        GetComponentInChildren<CameraController>().enabled = true;
+    }
 
     void Awake() {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        originalRotation = transform.localRotation;
+
         rigidbody = GetComponent<Rigidbody>();
-        camController = GetComponentInChildren<CameraController>();
+        cam = GetComponentInChildren<Camera>();
         rigidbody.freezeRotation = true;
         rigidbody.useGravity = false;
     }
 
     void FixedUpdate() {
+        if (!isLocalPlayer) return;
+
+        DoRotation();
         if (grounded) {
             // Calculate how fast we should be moving
             Vector3 targetVelocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-            targetVelocity = camController.transform.TransformDirection(targetVelocity);
+            //Put it into WorldSpace
+            targetVelocity = transform.TransformDirection(targetVelocity);
             targetVelocity *= speed;
 
             // Apply a force that attempts to reach our target velocity
             Vector3 velocity = rigidbody.velocity;
             Vector3 velocityChange = (targetVelocity - velocity);
+            //Clamp it at the maxVelocityChange
             velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
             velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
             velocityChange.y = 0;
-            rigidbody.AddRelativeForce(velocityChange, ForceMode.VelocityChange);
+            //AddForce, because we already put it into WorldSpaceCoordinates, otherwise we had to use AddRelativeForce
+            rigidbody.AddForce(velocityChange, ForceMode.VelocityChange);
 
             // Jump
             if (canJump && Input.GetButton("Jump")) {
                 rigidbody.velocity = new Vector3(velocity.x, CalculateJumpVerticalSpeed(), velocity.z);
+            }
+
+            //Unlock the Cursor when the user press escape
+            if(Input.GetKey(KeyCode.Escape)) {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
             }
         }
 
@@ -59,5 +88,26 @@ public class PlayerControllerRigidbody : NetworkBehaviour {
         // From the jump height and gravity we deduce the upwards speed 
         // for the character to reach at the apex.
         return Mathf.Sqrt(2 * jumpHeight * gravity);
+    }
+
+    //Ensure that we rotate the player itself so we use the rotation stuff here
+    private void DoRotation() {
+        rotationX += Input.GetAxis("Mouse X") * sensitivityX;
+        rotationX = ClampAngle(rotationX, -360, 360);
+        Quaternion xQuaternion = Quaternion.AngleAxis(rotationX, Vector3.up);
+        transform.localRotation = originalRotation * xQuaternion;
+    }
+
+    public static float ClampAngle(float angle, float min, float max) {
+        angle = angle % 360;
+        if ((angle >= -360F) && (angle <= 360F)) {
+            if (angle < -360F) {
+                angle += 360F;
+            }
+            if (angle > 360F) {
+                angle -= 360F;
+            }
+        }
+        return Mathf.Clamp(angle, min, max);
     }
 }
